a69e596d9e7eb3cbc7c226787042cbc5fcfb8deb
Wason1797 <wabrborich@hotmail.com>
2021-05-03 18:03:20 -0500

    add PostgreSqlConnector
    * This connector allows to manage the database dependency required by FastAPI
    
    * We are using the new async features provided by SQLAlchemy 1.4
    
    * Note that the main declarative_base is publicly accessible, this will allow us to use its metadata for the migrations

---
+++ app/db/postgre_connector/connector.py
@@ -0,0 +1,15 @@
+from app.db import Base
+from app.utils.env_manager import EnvManager
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.orm import sessionmaker
+
+
+class PostgreSqlConnector:
+    engine = create_async_engine(EnvManager.DB_URL, echo=True)
+    SessionLocal = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+    Basse = Base
+
+    @classmethod
+    async def get_db(cls) -> AsyncSession:
+        async with cls.SessionLocal() as session:
+            yield session
