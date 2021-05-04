7ab22a57ed3a34d13cdca6c751cbf2f5d1c79844
Wason1797 <wabrborich@hotmail.com>
2021-05-04 17:23:52 -0500

    Fix wrong env variable lookup

--- app/utils/env_manager.py
+++ app/utils/env_manager.py
@@ -3,5 +3,5 @@ import os
 
 class EnvManager:
     ENV: str = os.environ.get('ENV', 'DEV')
-    DB_URL: str = os.environ.get('AUTH_API_DB_URL', '').strip()
+    DB_URL: str = os.environ.get('DB_URL', '').strip()
     TESTING_DB_URL:  str = os.environ.get('TESTING_DB_URL', '').strip()
