7c0062e981ede668785ed73c4f024dad30a66698
Wason1797 <wabrborich@hotmail.com>
2021-05-04 17:06:56 -0500

    Rework the initialization architecture for the database engine
    * As we are using different engines for migrations (psycopg2) and (asyncpg) for the actual API I changed the engine initialization to happen on demand.
    
    * This will allow the API to initialize the database engine on startup
    
    * Also we are not creating a session or engine the first time we call the PostgreSqlConnector class

--- app/db/postgre_connector/connector.py
+++ app/db/postgre_connector/connector.py
@@ -1,3 +1,4 @@
+from sqlalchemy.ext.asyncio.engine import AsyncEngine
 from app.db import Base
 from app.utils.env_manager import EnvManager
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
@@ -5,11 +6,23 @@ from sqlalchemy.orm import sessionmaker
 
 
 class PostgreSqlConnector:
-    engine = create_async_engine(EnvManager.DB_URL, echo=True)
-    SessionLocal = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+    engine: AsyncEngine = None
+    SessionLocal: AsyncSession = None
     Base = Base
 
     @classmethod
+    def init_db_engine(cls) -> None:
+        if not cls.engine:
+            cls.engine = create_async_engine(EnvManager.DB_URL, echo=True)
+
+    @classmethod
+    def init_db_session(cls) -> None:
+        if not cls.SessionLocal:
+            cls.init_db_engine()
+            cls.SessionLocal = sessionmaker(cls.engine, class_=AsyncSession, expire_on_commit=False)
+
+    @classmethod
     async def get_db(cls) -> AsyncSession:
+        cls.init_db_session()
         async with cls.SessionLocal() as session:
             yield session
