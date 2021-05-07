bf05e50b5b96645ca9230339a4b220b9e45e4af2
Wason1797 <wabrborich@hotmail.com>
2021-05-06 19:15:48 -0500

    Delete env_manager.py

--- app/utils/env_manager.py
+++
@@ -1,7 +0,0 @@
-import os
-
-
-class EnvManager:
-    ENV: str = os.environ.get('ENV', 'DEV')
-    DB_URL: str = os.environ.get('DB_URL', '').strip()
-    TESTING_DB_URL:  str = os.environ.get('TESTING_DB_URL', '').strip()
