9f5fa94175342eb9955b6961843e19b05829e19e
Wason1797 <wabrborich@hotmail.com>
2021-05-06 19:10:28 -0500

    Refactor EnvManager -> EnvConfig

--- app/db/postgre_connector/connector.py
+++ app/db/postgre_connector/connector.py
@@ -1,11 +1,11 @@
 from app.db import Base
-from app.utils.env_manager import EnvManager
+from app.utils.env_config import EnvConfig
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker
 
 
 class PostgreSqlConnector:
-    engine = create_async_engine(EnvManager.DB_URL, echo=True)
+    engine = create_async_engine(EnvConfig.DB_URL, echo=True)
     SessionLocal = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
     Base = Base
 
