f0d6bf2c83c3e61ca600cc21c953ed4a65852098
Wason1797 <wabrborich@hotmail.com>
2021-05-16 18:21:42 -0500

    Add configurations to test endpoints with authettication
    * Monkeypatch AuthFunctions with mock methods so they allow testing without a valid token
    
    * Add a global fixture to get the db session function for SQLite in case of needing it
    
    * Override the auth user dependency so endpoints do not require authentication

--- app/tests/conftest.py
+++ app/tests/conftest.py
@@ -5,16 +5,45 @@ from app.db.postgre_connector import PostgreSqlConnector
 from app.db.sqlite_connector import SqLiteConnector
 from app.main import app
 from fastapi.testclient import TestClient
+from app.security import auth_user, AuthFunctions
 
 # Import fixtures to make them avilable for all tests
 from app.tests.fixtures.user import *
 
 app.dependency_overrides[PostgreSqlConnector.get_db] = SqLiteConnector.get_db
+app.dependency_overrides[auth_user] = {}
 
 
 @pytest.fixture
 def client():
     yield TestClient(app)
+
+
+def mock_check_permission_mapping_by_uuid(*args, **kwargs):
+    pass
+
+
+def mock_check_own_permission_by_uuid(*args, **kwargs):
+    pass
+
+
+@pytest.fixture
+def db_session():
+    return SqLiteConnector.get_db
+
+
+@pytest.fixture(autouse=True)
+def patch_auth_functions(monkeypatch):
+
+    monkeypatch.setattr(
+        AuthFunctions,
+        'check_permission_mapping_by_uuid',
+        mock_check_permission_mapping_by_uuid)
+
+    monkeypatch.setattr(
+        AuthFunctions,
+        'check_own_permission_by_uuid',
+        mock_check_own_permission_by_uuid)
 
 
 def pytest_configure(config):
