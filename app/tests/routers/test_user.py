e0bd7794885ff35aaae970583c10244cb1b71347
Wason1797 <wabrborich@hotmail.com>
2021-05-28 17:14:25 -0500

    add tests for user basic update and deletion

--- app/tests/routers/test_user.py
+++ app/tests/routers/test_user.py
@@ -16,3 +16,28 @@ def test_get_user_by_uuid(client, create_user):
     assert 'password' not in response_user
     for key in user:
         assert response_user[key] == user[key]
+
+
+def test_user_basic_update(client, create_user):
+    new_phone = '0123456789'
+    user = create_user.json()
+    response = client.patch(
+        '/user',
+        json={
+            'uuid': user['uuid'],
+            'phone': new_phone
+        }
+    )
+    updated_user = response.json()
+    assert response.status_code == 200
+    assert updated_user['phone'] == new_phone
+
+
+def test_delete_user(client, create_user):
+    user = create_user.json()
+    uri = f"/user/{user['uuid']}"
+    response = client.delete(uri)
+    check_deleted_user = client.get(uri)
+    assert response.status_code == 200
+    assert response.json()['uuid'] == user['uuid']
+    assert not check_deleted_user.json()
