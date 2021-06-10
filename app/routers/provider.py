9097503f0a5a3aff9797607a6f822ea87f26a648
Wason1797 <wabrborich@hotmail.com>
2021-06-10 17:59:03 -0500

    fix wrong calls to user methods inside provider router

--- app/routers/provider.py
+++ app/routers/provider.py
@@ -3,7 +3,7 @@ from app.db.postgre_connector import PostgreSqlConnector
 from app.managers.provider import ProviderManager
 from app.serializers.provider import ProviderCreate as ProviderCreateSerializer
 from app.serializers.provider import ProviderUpdate as ProviderUpdateSerializer
-from app.serializers.provider import provider as ProviderSerializer
+from app.serializers.provider import Provider as ProviderSerializer
 from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 from app.utils.functions import filter_dict_keys
@@ -18,15 +18,15 @@ async def get_provider(uuid: str, db: AsyncSession = Depends(PostgreSqlConnector
 
 @router.post('', response_model=ProviderSerializer)
 async def create_provider(provider: ProviderCreateSerializer, db: AsyncSession = Depends(PostgreSqlConnector.get_db)):
-    return await ProviderManager.create_user(db, provider)
+    return await ProviderManager.create_provider(db, provider)
 
 
 @router.patch('', response_model=ProviderSerializer)
 async def basic_provider_update(provider: ProviderUpdateSerializer, db: AsyncSession = Depends(PostgreSqlConnector.get_db)):
     update_values = filter_dict_keys(provider.dict(), {'uuid'}, prune_null=True)
-    return await ProviderManager.uppdate_provider_by_uuid(db, provider.uuid, update_values)
+    return await ProviderManager.update_provider_by_uuid(db, provider.uuid, update_values)
 
 
 @router.delete('/{uuid}', response_model=ProviderSerializer)
 async def delete_provider(uuid: str, db: AsyncSession = Depends(PostgreSqlConnector.get_db)):
-    return await ProviderManager.delete_user(db, uuid)
+    return await ProviderManager.delete_provider(db, uuid)
