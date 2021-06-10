2394ce2c178b080b7d1a86eb671addd972da378c
Wason1797 <wabrborich@hotmail.com>
2021-06-10 17:31:21 -0500

    add provider router and endpoints
    * add endpoints to fetch, create, update and delete a provider

---
+++ app/routers/provider.py
@@ -0,0 +1,32 @@
+
+from app.db.postgre_connector import PostgreSqlConnector
+from app.managers.provider import ProviderManager
+from app.serializers.provider import ProviderCreate as ProviderCreateSerializer
+from app.serializers.provider import ProviderUpdate as ProviderUpdateSerializer
+from app.serializers.provider import provider as ProviderSerializer
+from fastapi import APIRouter, Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+from app.utils.functions import filter_dict_keys
+
+router = APIRouter()
+
+
+@router.get('/{uuid}', response_model=ProviderSerializer)
+async def get_provider(uuid: str, db: AsyncSession = Depends(PostgreSqlConnector.get_db)):
+    return await ProviderManager.fetch_by_uuid(db, uuid)
+
+
+@router.post('', response_model=ProviderSerializer)
+async def create_provider(provider: ProviderCreateSerializer, db: AsyncSession = Depends(PostgreSqlConnector.get_db)):
+    return await ProviderManager.create_user(db, provider)
+
+
+@router.patch('', response_model=ProviderSerializer)
+async def basic_provider_update(provider: ProviderUpdateSerializer, db: AsyncSession = Depends(PostgreSqlConnector.get_db)):
+    update_values = filter_dict_keys(provider.dict(), {'uuid'}, prune_null=True)
+    return await ProviderManager.uppdate_provider_by_uuid(db, provider.uuid, update_values)
+
+
+@router.delete('/{uuid}', response_model=ProviderSerializer)
+async def delete_provider(uuid: str, db: AsyncSession = Depends(PostgreSqlConnector.get_db)):
+    return await ProviderManager.delete_user(db, uuid)
