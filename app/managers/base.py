a513d877ef25fb716207b280783f268bacee5d50
Wason1797 <wabrborich@hotmail.com>
2021-06-10 17:24:55 -0500

    add abstraction to execute updates in BaseManager
    * Now that we have updates for providers it is necessary to add an abstraction for the update function so we have a clean implementation in each of the managers

--- app/managers/base.py
+++ app/managers/base.py
@@ -1,5 +1,7 @@
-from typing import Any
+from typing import Any, Coroutine, Iterable, Union
 
+from app.utils.constants import DbDialects
+from app.utils.functions import build_from_key_value_arrays
 from sqlalchemy.engine.result import Result
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.sql.base import Executable
@@ -23,3 +25,16 @@ class BaseManager:
         session_add = db.add_all if isinstance(obj, list) else db.add
         session_add(obj)
         return obj
+
+    @classmethod
+    async def execute_update_stmt_by_uuid(cls, db: AsyncSession, update_stmt: Executable, columns: Iterable,
+                                          fetch_coro: Coroutine, uuid: str) -> Union[dict, type(model)]:
+        if db.bind.dialect.name == DbDialects.POSTGRESQL.value:
+            result = (await cls.execute_stmt(db, update_stmt.returning(*columns))).first()
+            await db.commit()
+            return build_from_key_value_arrays(columns.keys(), result)
+        else:
+            await cls.execute_stmt(db, update_stmt)
+            await db.commit()
+            result = await fetch_coro(db, uuid, filter_status=None)
+            return result
